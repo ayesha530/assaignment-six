748e7b9156221c2851faf50ce7f63dfd38b81ba4
ayesha530 <ayeshabd200@gmail.com>
2025-09-08 14:16:36 +0600

    added card section and plant function

--- script/index.js
+++ script/index.js
@@ -5,13 +5,13 @@ const loadCategories = () => {
         .catch(error => console.error(error));
 }
 const displayCategories = (categories) => {
-    
+
     const cardContainer = document.getElementById('card_container');
     cardContainer.innerHTML = '';
     for (let category of categories) {
         const loadDiv = document.createElement('div');
         loadDiv.innerHTML = `
-        <button class="btn btn-xs sm:btn-sm  xl:btn-xl lg:w-[250px] mb-4">${category.category_name}</button>
+        <button class="btn lg:w-[200px] mb-4">${category.category_name}</button>
        `
         cardContainer.append(loadDiv)
     }
@@ -24,19 +24,43 @@ const loadPlant = () => {
 }
 const displayPlant = (plants) => {
     const plantContainer = document.getElementById('plant_container');
-    
-     plants.forEach(plant => {
-      const plantDiv = document.createElement('div');
-      plantDiv.innerHTML = `
+
+    plants.forEach(plant => {
+        const plantDiv = document.createElement('div');
+        plantDiv.innerHTML = `
+      <div class="bg-base-200  shadow-sm p-3">
+                    <figure class="h-[200px]">
+                        <img src="${plant.image}"
+                            alt="${plant.image}" class="h-full w-full object-cover"/>
+                    </figure>
+                    <div class="mt-3">
+                        <h2 class="text-[#1F2937] text-[14px] font-semibold font-inter mb-3">${plant.name} </h2>
+                        <p class="text-[#1F2937] text-xs font-inter mb-3">${plant.description.length > 80 ? plant.description.slice(0, 80) + '...' : plant.description}
+                        </p>
+                        
+                        <div class="flex justify-between">
+                           <div class="badge bg-[#DCFCE7] text-[#15803D] text-[14px] font-medium">
+                           ${plant.category}
+                           </div>
+                           <div class="badge text-[#1F2937] text-[14px] font-semibold font-inter">
+                          ${plant.price}
+                          </div>
+                          </div>
+                        
+                        <div class="mt-3">
+                            <button class="w-full bg-[#15803D] rounded-[15px] py-3 text-[16px] font-inter font-medium text-[#fff]">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
       
       `
-      plantContainer.append(plantDiv)
-        
-     });
-   
+        plantContainer.append(plantDiv)
 
-  
-    
+    });
+
+
+
+
 
 }
 loadCategories()
