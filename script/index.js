d760ed6a81bb131000f4d1090c2823f1eab4021c
ayesha530 <ayeshabd200@gmail.com>
2025-09-09 09:55:03 +0600

    add click event to category button

--- script/index.js
+++ script/index.js
@@ -11,20 +11,85 @@ const displayCategories = (categories) => {
     for (let category of categories) {
         const loadDiv = document.createElement('div');
         loadDiv.innerHTML = `
-        <button class="btn lg:w-[200px] mb-4">${category.category_name}</button>
+        <button class="btn lg:w-[200px] mb-4" 
+        id="categoryBtn-${category.id}" 
+        onclick="loadshowCategory(${category.id})"
+        >
+        ${category.category_name}
+        </button>
        `
         cardContainer.append(loadDiv)
     }
 
 }
+// category found in id
+const loadshowCategory = (id) =>{
+       const url = (`https://openapi.programming-hero.com/api/category/${id}`)
+       fetch(url)
+       .then(res => res.json())
+       .then(data => {
+            // Hide all plants container
+            document.getElementById('plant_container').style.display = 'none';
+            // Show category container
+            const categoryContainer = document.getElementById('category_container');
+            categoryContainer.style.display = 'grid'
+            categoryContainer.innerHTML = '';
+
+           
+            displayShowCategories(data.plants);
+        });
+};
+
+ const displayShowCategories = (plants) =>{
+    console.log(plants)
+     const categoryContainer = document.getElementById('category_container');
+   
+
+   plants.forEach(plant => {
+      const plantDiv = document.createElement('div');
+      plantDiv.innerHTML = `
+          <div class="bg-base-200  shadow-sm p-3">
+                    <figure class="h-[200px]">
+                        <img src="${plant.image}"
+                            alt="${plant.image}" class="h-full w-full object-cover"/>
+                    </figure>
+                    <div class="mt-3">
+                        <h2 class="text-[#1F2937] text-[14px] font-semibold font-inter mb-3">${plant.name} </h2>
+                        <p class="text-[#1F2937] text-xs font-inter mb-3">${plant.description.length > 80 ? plant.description.slice(0, 80) + '...' : plant.description}
+                        </p>
+                        
+                        <div class="flex justify-between">
+                           <div class="badge bg-[#DCFCE7] text-[#15803D] text-[14px] font-medium">
+                           ${plant.category}
+                           </div>
+                           <div class="badge text-[#1F2937] text-[14px] font-semibold font-inter">
+                          ${plant.price}
+                          </div>
+                          </div>
+                        
+                        <div class="mt-3">
+                            <button class="w-full bg-[#15803D] rounded-[15px] py-3 text-[16px] font-inter font-medium text-[#fff]">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+        
+      `
+     categoryContainer.append(plantDiv);
+   });
+
+
+}
+// All Plant show start
 const loadPlant = () => {
     fetch('https://openapi.programming-hero.com/api/plants')
         .then(res => res.json())
         .then(data => displayPlant(data.plants))
 }
 const displayPlant = (plants) => {
+    
     const plantContainer = document.getElementById('plant_container');
 
+     plantContainer.innerHTML = '';
     plants.forEach(plant => {
         const plantDiv = document.createElement('div');
         plantDiv.innerHTML = `
@@ -63,5 +128,11 @@ const displayPlant = (plants) => {
 
 
 }
+// Show all plants again
+const showAllPlants = () => {
+    document.getElementById('plant_container').style.display = 'grid';
+    document.getElementById('category_container').style.display = 'none';
+};
+// All plant show end
 loadCategories()
 loadPlant()
